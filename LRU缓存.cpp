b19caafdca361838beced817eca6cfced5c28c62
superGYC <104135371+superfuckergyc@users.noreply.github.com>
2026-03-09 22:14:31 +0800

    Create LRU缓存.cpp

---
+++ LRU缓存.cpp
@@ -0,0 +1,69 @@
+struct Node {
+    int key;
+    int value;
+    Node* prev;
+    Node* next;
+
+    Node(int k = 0, int v = 0) : key(k), value(v) {}
+};
+
+class LRUCache {
+private:
+    int capacity;
+    Node* dummy; // 哨兵节点
+    unordered_map<int, Node*> key_to_node;
+
+    // 删除一个节点（抽出一本书）
+    void remove(Node* x) {
+        x->prev->next = x->next;
+        x->next->prev = x->prev;
+    }
+
+    // 在链表头添加一个节点（把一本书放到最上面）
+    void push_front(Node* x) {
+        x->prev = dummy;
+        x->next = dummy->next;
+        x->prev->next = x;
+        x->next->prev = x;
+    }
+
+    // 获取 key 对应的节点，同时把该节点移到链表头部
+    Node* get_node(int key) {
+        auto it = key_to_node.find(key);
+        if (it == key_to_node.end()) { // 没有这本书
+            return nullptr;
+        }
+        Node* node = it->second; // 有这本书
+        remove(node); // 把这本书抽出来
+        push_front(node); // 放到最上面
+        return node;
+    }
+
+public:
+    LRUCache(int capacity) : capacity(capacity), dummy(new Node()) {
+        dummy->prev = dummy;
+        dummy->next = dummy;
+    }
+
+    int get(int key) {
+        Node* node = get_node(key); // get_node 会把对应节点移到链表头部
+        return node ? node->value : -1;
+    }
+
+    void put(int key, int value) {
+        Node* node = get_node(key); // get_node 会把对应节点移到链表头部
+        if (node) { // 有这本书
+            node->value = value; // 更新 value
+            return;
+        }
+        key_to_node[key] = node = new Node(key, value); // 新书
+        push_front(node); // 放到最上面
+        if (key_to_node.size() > capacity) { // 书太多了
+            Node* back_node = dummy->prev;
+            key_to_node.erase(back_node->key);
+            remove(back_node); // 去掉最后一本书
+            delete back_node; // 释放内存
+        }
+    }
+};
+
